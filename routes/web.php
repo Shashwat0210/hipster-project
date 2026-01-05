7b064abc4eaa77c584165b7f6882d2b23d6f1b3f
Shashwat Shukla <ss.shukla599@gmail.com>
2026-01-05 13:17:59 +0530

    Admin and Customer models, migrations and mutli-auth guards setup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth:admin')->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+});
+
+Route::middleware('auth:customer')->group(function () {
+    Route::get('/customer/dashboard', function () {
+        return 'Customer Dashboard';
+    });
 });
